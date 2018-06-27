bcf7ab5ea39d49dcf242c30997898bb1b0e2a684
Colin Harris <col.w.harris@gmail.com>
2018-06-27 22:24:11 +0800

    First PoC

--- PhotoWall/ViewController.swift
+++ PhotoWall/ViewController.swift
@@ -13,63 +13,73 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var videoPlayer: AVPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+//        sceneView.showsStatistics = true
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        if let url = Bundle.main.url(forResource: "Col", withExtension: "mp4", subdirectory: "art.scnassets") {
+            videoPlayer = AVPlayer(url: url)
+            
+            NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: videoPlayer.currentItem, queue: nil) { _ in
+                self.videoPlayer.seek(to: .zero)
+                self.videoPlayer.play()
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    
+        let configuration = ARImageTrackingConfiguration()
+        guard let referencesImages = ARReferenceImage.referenceImages(inGroupNamed: "Photos", bundle: nil) else {
+            print("Could not load photos!")
+            return
+        }
+        configuration.trackingImages = referencesImages
+        configuration.maximumNumberOfTrackedImages = 1
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
      
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
+                                 height: imageAnchor.referenceImage.physicalSize.height)
+                
+            plane.firstMaterial?.diffuse.contents = self.videoPlayer
+            self.videoPlayer.play()
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+        }
+
         return node
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
